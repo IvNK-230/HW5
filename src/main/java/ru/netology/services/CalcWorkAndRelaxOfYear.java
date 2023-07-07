cb1e42e87bb6a7478c3d82d510d7320e43755fe6
Ivan Kashuba <kashubaim@gmail.com>
2023-07-07 23:29:29 +0600

    second_new

--- src/main/java/ru/netology/services/CalcWorkAndRelaxOfYear.java
+++ src/main/java/ru/netology/services/CalcWorkAndRelaxOfYear.java
@@ -3,20 +3,15 @@ package ru.netology.services;
 public class CalcWorkAndRelaxOfYear {
     public int calculate(int income, int expenses, int threshold) {
         int cuont = 0;                                        // счётчик месяцев отдыха
-        int money = 0;                                        // колличество средств перед отпуском
         int wallet = 0;                                       // количество денег на счету
-        int rest = 0;                                         // сумма расходов в отпуске
 
         for (int month = 0; month < 12; month++) {
             if (wallet >= threshold) {                       // можем ли отдыхать?
-                money = wallet;                              // передаём сумму средств перед отпуском
-                rest = wallet * 2;                           // сумма средств потраченных в отпуске
-                wallet = (wallet - expenses) / 3;            // оставшиеся средства после опуска
-                System.out.println("Месяц " + (month + 1) + ". Денег " + money + ". Буду отдыхать. Потратил - " + expenses + ", затем еще " + rest);
+                wallet -= expenses;
+                wallet /= 3;
                 cuont++;
 
             } else {
-                System.out.println("Месяц " + (month + 1) + ". Денег " + wallet + ". Придется работать. Заработал + " + income + ", потратил -" + expenses);
                 wallet = wallet + income - expenses;         // сумма накопленных средств
             }
         }
